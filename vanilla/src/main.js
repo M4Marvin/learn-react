7cd1dd9e815a9ad9d1f0690c6ee083c4d32d2459
Calvin <calvinvprakash@gmail.com>
2024-12-16 14:45:26 +0530

    Added Todo list

--- vanilla/src/main.js
+++ vanilla/src/main.js
@@ -1,23 +1,57 @@
 import './style.css'
 
-const buttonIncrement = document.getElementById('increment')
-const buttonDecrement = document.getElementById('decrement')
-const counterDisplay = document.getElementById('display')
+const addInput = document.getElementById("todo-input")
+const todoForm = document.getElementById("todo-form")
+const todoList = document.getElementById("todo-list")
 
-let count = 1
-counterDisplay.innerHTML = count
 
-const increase = () => {
-  count ++;
-  counterDisplay.innerHTML = count
-  console.log("inc 1")
-} ;
 
-const decrease = () => {
-  count --;
-  counterDisplay.innerHTML = count
-  console.log("dec 1")
-} ;
+const todos = ["Wake up", "Eat food"]
+const renderTodos = () => {
+  // Todo :  take each todos and add it to the list one by one
+  todoList.innerHTML = ""
+  for (const todo of todos){
+    const li = document.createElement("li")
+    li.innerHTML= todo
+    todoList.appendChild(li)
+  }
+}
 
-buttonIncrement.addEventListener("click", increase)
-buttonDecrement.addEventListener("click", decrease)
+const appendTodo = () => {
+  // Get input and add in todos
+  todos.push(addInput.value)
+  console.log(todos)
+}
+
+const addTodo = (event) => {
+  event.preventDefault();
+  console.log(addInput.value)
+
+  appendTodo()
+  renderTodos()
+}
+
+todoForm.addEventListener("submit", addTodo)
+
+renderTodos()
+// const buttonIncrement = document.getElementById('increment')
+// const buttonDecrement = document.getElementById('decrement')
+// const counterDisplay = document.getElementById('display')
+
+// let count = 1
+// counterDisplay.innerHTML = count
+
+// const increase = () => {
+//   count ++;
+//   counterDisplay.innerHTML = count
+//   console.log("inc 1")
+// } ;
+
+// const decrease = () => {
+//   count --;
+//   counterDisplay.innerHTML = count
+//   console.log("dec 1")
+// } ;
+
+// buttonIncrement.addEventListener("click", increase)
+// buttonDecrement.addEventListener("click", decrease)
