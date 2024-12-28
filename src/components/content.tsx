57516ef6bccc86192cbfbdfff22c01c9d068f84e
Cylently <calvinvprakash@gmail.com>
2024-12-28 18:02:30 +0530

    Added navbar,footer,header, content and some layouting

---
+++ src/components/content.tsx
@@ -0,0 +1,32 @@
+function Content() {
+  return (
+    <div className="p-3">
+      <div className="text-2xl">Installation</div>
+      <div>
+        The simplest and fastest way to get up and running with Tailwind CSS
+        from scratch is with the Tailwind CLI tool. The CLI is also available as
+        a standalone executable if you want to use it without installing
+        Node.js. Install Tailwind CSS Install tailwindcss via npm, and create
+        your tailwind.config.js file.
+      </div>
+      <br></br>
+      <div className="text-2xl">What to read next</div>
+      <div>
+        Get familiar with some of the core concepts that make Tailwind CSS
+        different from writing traditional CSS. Utility-First Fundamentals Using
+        a utility-first workflow to build complex components from a constrained
+        set of primitive utilities. Responsive Design Build fully responsive
+        user interfaces that adapt to any screen size using responsive
+        modifiers. Hover, Focus & Other States Style elements in interactive
+        states like hover, focus, and more using conditional modifiers. Dark
+        Mode Optimize your site for dark mode directly in your HTML using the
+        dark mode modifier. Reusing Styles Manage duplication and keep your
+        projects maintainable by creating reusable abstractions. Customizing the
+        Framework Customize the framework to match your brand and extend it with
+        your own custom styles.
+      </div>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <div className="flex justify-between space-x-3 p-3">
+      <div className="flex space-x-2">
+        <div>Name, logo</div>
+        <div>Version </div>
+        <div>Notification </div>
+      </div>
+      <div className="flex space-x-2">
+        <div>Docs </div>
+        <div>Components </div>
+        <div>Blog </div>
+        <div>Showcase </div>
+        <div>Theme </div>
+        <div>Profile </div>
+      </div>
+    </div>
+  );
+}
+export default Header;
